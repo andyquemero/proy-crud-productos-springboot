8dfdbeb1747fd8ba2e7d018807fccb82498fa2f7
andyquemero <48929857+andyquemero@users.noreply.github.com>
2020-12-12 18:43:12 -0300

    se sube arquitectura base del proyecto crud de productos

---
+++ src/main/java/com/acosta/springboot/app/ProyCrudProductosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.acosta.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyCrudProductosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyCrudProductosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acosta/springboot/app/ProyCrudProductosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acosta.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyCrudProductosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
